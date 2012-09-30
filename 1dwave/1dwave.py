e03f99d9da76859808ae72716ab0d2befdf835f2
Andreas Solbrå <andreas_vso@hotmail.com>
2012-09-30 17:20:00 +0200

    1dwave: fixed bug, added reflective boundaries (del u/ del n = 0), added different propagation speeds, added colored backround

--- 1dwave/1dwave.py
+++ 1dwave/1dwave.py
@@ -5,85 +5,112 @@ import numpy as np
 import subprocess                 # For issuing commands to the OS.
 import os
 import sys                        # For determining the Python version.
+import matplotlib.patches as mpatches
 
 t0 = 0.0
-T = 1.2
-c = 3.5
-dt = 0.01
-dx = 0.05
+T = 200.0
+dt = 0.1
+dx = 0.1
 Nt = int((T-t0)/dt)
-x0 = 0.0
-x1 = 1.0
-
+x0 = -20.
+x1 = 20.
 Nx = int((x1-x0)/dx)
 t = np.linspace(t0,T,Nt)
-x_init = np.zeros(Nx)
-x = np.zeros(Nx)
-xp = np.zeros(Nx)
-xpp = np.zeros(Nx)
-dxdt = np.zeros(Nx)
-dxdtp = np.zeros(Nx)
 
-factor = c*c*dt*dt/(dx*dx)
-
-#initial positions
-xpp = np.linspace(x0,x1,Nx)
-xp = (xpp-x0)*(xpp-x1)
 
 xplot = np.linspace(x0,x1,Nx)
+x = np.linspace(x0,x1,Nx)
+xp = np.zeros(Nx)
+xpp = np.zeros(Nx)
 
-plt.plot(xpp,xp)
+#initial positions
+sigma = 2.0
+xinit = np.exp(-x*x/(sigma*sigma))
 
+def factor(x,t):
+    factor = np.logical_and((x-x0)/(x1-x0) >= 2./3.,((x-x0)/(x1-x0) <= 4./5.))*0.4 + \
+        np.logical_or((x-x0)/(x1-x0) <= 2./3., (x-x0)/(x1-x0)>= 4./5.)*0.3
+    return factor*factor*dt*dt/(dx*dx)
 
 #first step
 for i in range(1,Nx-1):
-	x[i] = xp[i] + (factor/2.)*(xp[i+1] - 2*xp[i] + x[i+1])
-xpp = xp
-xp = x
+	x[i] =  xinit[i] + (factor(xplot[i], t[0])/2.)*(xinit[i+1] - 2*xinit[i] + xinit[i-1])
+x[0] = 0.
+x[-1] = 0.
+xpp = np.copy(xinit)
+xp = np.copy(x)
+
+"""
+plt.plot(xplot,dxdtinit)
+plt.hold('on')
+plt.show()
+"""
+
+# Some rectangles for visualization
+# first from 0 to 2/3: 
+xy = np.array([x0,-1.3])
+height = 2.6*(x1-x0)
+width = (2./3.)*(x1-x0)
+q1 = mpatches.Rectangle(xy, width, height, facecolor='yellow', edgecolor='yellow')
+
+# then from 2/3 to 4/5:
+xy = np.array([x0+2./3.*(x1-x0),-1.3])
+height = 2.6*(x1-x0)
+width = (4./5.-2./3.)*(x1-x0)
+q2 = mpatches.Rectangle(xy, width, height, facecolor='cyan', edgecolor='cyan')
+
+#finally from 4/5 to 1
+xy = np.array([x0+4./5.*(x1-x0),-1.3])
+height = 2.6*(x1-x0)
+width = (1.-4./5.)*(x1-x0)
+q3 = mpatches.Rectangle(xy, width, height, facecolor='yellow', edgecolor='yellow')
 
 
 for n in range(Nt):
-    for i in range(1,Nx-1):
-        x[i] = 2*xp[i] - xpp[i] + factor*(xp[i-1] - 2*xp[i] + xp[i+1])
-    x[0] = 0.0
-    x[-1] = 0.0
-    xpp = xp
-    xp = x
-    plt.plot(xplot,x)
-    plt.axis((x0,x1, abs(x0-x1),-abs(x0-x1)))
-    #
-    # The next four lines are just like MATLAB.
-    #
-    plt.plot(xplot,x,'b.')
-    plt.axis((x0,x1,-abs(x0-x1), abs(x0-x1)))
-    plt.xlabel('x-position')
-    plt.ylabel('y-position')
+#    for i in range(1,Nx-1):
+#        x[i] = 2*xp[i] - xpp[i] + factor*(xp[i-1] - 2*xp[i] + xp[i+1])
+    x[1:-1] = (2*xp[1:-1] - xpp[1:-1] + factor(xplot[1:-1],t[n])*(xp[:-2] - 2*xp[1:-1] + xp[2:]))
+    x[0] = x[1] #(2*(1-factor(xplot[0], t[n]) ))*xp[0] - xpp[0] + factor(xplot[0], t[n])*xp[1];
+    x[-1] = x[-2] #(2*(1-factor(xplot[-1],t[n]) ))*xp[-1] - xpp[-1] + factor(xplot[0],t[n])*xp[-2];
+    xpp = np.copy(xp)
+    xp = np.copy(x)
+    if np.mod(n,10) == 0:
+	    #
+	    # The next four lines are just like MATLAB.
+	    #
+            plt.gca().add_patch(q1)
+            plt.gca().add_patch(q2)
+            plt.gca().add_patch(q3)
+	    plt.plot(xplot,x,'b',hold=True)
+	    plt.axis((x0,x1,-1.3, 1.3))
+	    plt.xlabel('x-position')
+	    plt.ylabel('y-position')
 
-    #
-    # Notice the use of LaTeX-like markup.
-    #
-    plt.title(r'$t=balle$', fontsize=20)
+	    #
+	    # Notice the use of LaTeX-like markup.
+	    #
+	    plt.title(r'$t=%06d$' % n, fontsize=20)
 
-    #
-    # The file name indicates how the image will be saved and the
-    # order it will appear in the movie.  If you actually wanted each
-    # graph to be displayed on the screen, you would include commands
-    # such as show() and draw() here.  See the matplotlib
-    # documentation for details.  In this case, we are saving the
-    # images directly to a file without displaying them.
-    #
-    filename = str('%03d' % n) + '.png'
-    plt.savefig(filename, dpi=100)
+	    #
+	    # The file name indicates how the image will be saved and the
+	    # order it will appear in the movie.  If you actually wanted each
+	    # graph to be displayed on the screen, you would include commands
+	    # such as show() and draw() here.  See the matplotlib
+	    # documentation for details.  In this case, we are saving the
+	    # images directly to a file without displaying them.
+	    #
+	    filename = str('%06d' % n) + '.png'
+	    plt.savefig(filename, dpi=100)
 
-    #
-    # Let the user know what's happening.
-    #
-    print 'Wrote file', filename
+	    #
+	    # Let the user know what's happening.
+	    #
+	    print 'Wrote file', filename
 
-    #
-    # Clear the figure to make way for the next image.
-    #
-    plt.clf()
+	    #
+	    # Clear the figure to make way for the next image.
+	    #
+	    plt.clf()
 
 #
 # Now that we have graphed images of the dataset, we will stitch them
@@ -118,5 +145,3 @@ subprocess.check_call(command)
 print "\n\n The movie was written to 'output.avi'"
 
 print "\n\n You may want to delete *.png now.\n\n"
-
-
