9210579d7f3161586fc07b185b12f9d31df83c9d
Anders Hafreager <anders@tapcat.is>
2012-10-04 17:45:09 +0200

    Created a function that reads a bmp-file and returns a matrix with values from 0 (black) to 1 (white)

--- 2danders/mcrtmv.py
+++ 2danders/mcrtmv.py
@@ -17,13 +17,13 @@ X,Y = np.meshgrid(x,y);
 #ml.options.offscreen = True
 size = 500,500
 fig = ml.figure(size= size);
-#fig.scene.anti_aliasing_frames=0
+fig.scene.anti_aliasing_frames=0
 extent = [0,Nx-1,0,Nx-1,-7,7]
 for i in range(frames):
 	ml.clf(figure=fig)
 	u = np.loadtxt('data/test.d%07d'%i);
 	u[0][0] = 7;
-	u[0]1[] = -7;
+	u[0][1] = -7;
 	fname = 'data/_tmp%07d.png' % i
 	ml.surf(x,y,u,extent=extent,figure=fig,vmax=0.7,vmin=-0.7)
 #	ml.axes(figure=fig,xlabel='x',ylabel='y',zlabel='z',extent=extent)
--- 2danders/wave.cpp
+++ 2danders/wave.cpp
@@ -9,6 +9,61 @@ using namespace arma;
 
 int Nr;
 
+imat readBMP(char* filename)
+{
+    int i;
+    FILE* f = fopen(filename, "rb");
+    unsigned char info[54];
+    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+
+    // extract image height and width from header
+    int fileSize = *(int*)&info[2];
+    
+    int width = *(int*)&info[18];
+    int height = *(int*)&info[22];
+    int pixels = width*height;
+    int bytesLeft = fileSize-54; // 54 bytes for the header, we need the rest
+    printf("Image is %d bytes\n",fileSize);
+    printf("Image contains %d pixels (%d,%d)\n",pixels,height,width);
+
+    unsigned char* data = new unsigned char[bytesLeft];
+    fread(data, sizeof(unsigned char), bytesLeft, f); // read the rest of the data at once
+    fclose(f);
+
+    mat img = zeros<mat>(height,width);
+
+    int pixelCount = 0;
+    int pixelIndex = 0;
+
+    int row = height-1; // BMP-files start with the lower left pixel, row for row
+    int col = 0;
+
+    for(i = 0; i < pixels; i++)
+    {
+    		int r = data[pixelIndex+2]; // RGB values are interchanged in the file format
+    		int g = data[pixelIndex+1];
+    		int b = data[pixelIndex];
+
+    		double avg = (r+g+b)/3.0/255.0; // If we have black/white only, the average is 0 (black) to 255 (white)
+    		img(row,col) = avg;
+
+    		pixelCount++;
+    		pixelIndex+=3; // Each pixel has 3 bytes, RGB
+    		
+    		if(++col == width) {
+    			// BMP-format is stupid since it wants each row to have 4n bytes, so it adds 
+    			// the remaining pixels before next row
+    			int padding = col % 4;
+
+    			col = 0;
+    			pixelIndex+=padding;
+    			row--;
+    		}
+    }
+
+    return img;
+}
+
 inline int idx(int i) {
 	return (i+10*Nr)%Nr;
 }
